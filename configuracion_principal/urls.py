62fb673b2a033643c74ee9e31a2a929b183eb235
hrsos <hrsossio71@gmail.com>
2026-02-10 21:06:42 -0300

    Fix: allow serving media files in production for testing

--- configuracion_principal/urls.py
+++ configuracion_principal/urls.py
@@ -45,5 +45,9 @@ urlpatterns = [
 handler404 = 'gestion_interna.views.error_404'
 handler500 = 'gestion_interna.views.error_500'
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.urls import re_path
+from django.views.static import serve
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
