f632d369f118110449a659dc561c8ef39204122a
hrsos <hrsossio71@gmail.com>
2026-02-24 22:37:09 -0300

    Cleanup: remove one-time migration commands from build.sh

--- build.sh
+++ build.sh
@@ -6,11 +6,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-# ONE-TIME: Flush existing data and load backup
-# Remove these 2 lines after the first successful deploy
-python manage.py flush --no-input
-python manage.py loaddata data_backup.json
-
 python create_admin.py
 
