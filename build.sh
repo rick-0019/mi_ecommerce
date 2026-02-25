c735191628d5f43196a3bb1307057f4862015db7
hrsos <hrsossio71@gmail.com>
2026-02-24 22:20:11 -0300

    Data migration: add product data backup (no auth data)

--- build.sh
+++ build.sh
@@ -7,3 +7,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python create_admin.py
+
+# ONE-TIME: Load local data backup into production DB
+# Remove this line after the first successful deploy
+python manage.py loaddata data_backup.json
